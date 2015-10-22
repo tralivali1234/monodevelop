58406b95e68c56c361e54f2512d26cf658759332
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-10-22 08:10:46 -0700

    Revert "[Windows] Implement custom resize borders."
    This reverts commit 1c4d2360867645877c4f5b44b9540e5ed9ad402b.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/DesktopService.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/DesktopService.cs
@@ -353,10 +353,5 @@ namespace MonoDevelop.Ide
 		{
 			PlatformService.PlaceWindow (window, x, y, width, height);
 		}
-
-		internal static bool RequiresCustomResizeBorders ()
-		{
-			return PlatformService.RequiresCustomResizeBorders ();
-		}
 	}
 }
