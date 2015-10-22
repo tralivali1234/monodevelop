9ab0029842ea2fc361e78848c92291700f6ddc8f
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-10-22 08:12:43 -0700

    Revert "[Windows] Get rid of Windows titlebar."
    This reverts commit 6d107a1297b8aa63e9e8819d6e178b030823d5fe.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/DefaultWorkbench.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/DefaultWorkbench.cs
@@ -612,7 +612,7 @@ namespace MonoDevelop.Ide.Gui
 					FullScreen = memento.FullScreen;
 					toolbarFrame.SetStatus (memento.ToolbarStatus);
 				}
-				Decorated = !Platform.IsWindows;
+				Decorated = true;
 			}
 		}
 		
